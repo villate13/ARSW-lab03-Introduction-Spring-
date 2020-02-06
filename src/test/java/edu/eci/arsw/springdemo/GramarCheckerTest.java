54693c8fdfd42604dcdc6be0dcd64dff4f75ad27
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2020-02-06 09:52:22 -0500

    Prueba Espanol (funcionando)

--- src/test/java/edu/eci/arsw/springdemo/GramarCheckerTest.java
+++ src/test/java/edu/eci/arsw/springdemo/GramarCheckerTest.java
@@ -7,6 +7,8 @@ package edu.eci.arsw.springdemo;
 
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  *
@@ -14,8 +16,13 @@ import org.junit.Test;
  */
 public class GramarCheckerTest {
     @Test
-    public void deberiaCrearUnaLinkedList() {
+    public void probarGrammarEspanol() {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+        GrammarChecker gc = ac.getBean(GrammarChecker.class);
         
+        String correct;
+        correct="Spell checking output:revisando (la la la) con el verificador de sintaxis del espanolPlagiarism checking output: Not available yet";
+        assertTrue(correct.equals(gc.check("la la la")));
 
     }
 }
